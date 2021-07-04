1acd2281af914a5f6fa119428d79ab6536f23401
SylverSlide <nassim_zitouni@yahoo.com>
2021-07-04 15:55:49 +0200

    Refactoring Pin by adding Timestampable trait and delete attribut createdAt and UpdatedAt

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -31,16 +31,6 @@ class Pin
      */
     private $description;
 
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -70,42 +60,5 @@ class Pin
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-    
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function updateTimestamps(){
-        if ($this->getCreatedAt() === null ){
-            $this->setCreatedAt(new \DateTimeImmutable);
-        }
-        
-        $this->setUpdatedAt(new \DateTimeImmutable);
-
-        return '0';
-    }
 }
